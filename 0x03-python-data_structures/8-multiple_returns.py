b6121ad4da6532ae64d225cd5c984a99b1ad6865
David Adejumo <davidadejumo@gmail.com>
2022-09-26 19:19:51 -0700

    Data struct

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,8 +1,8 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 def multiple_returns(sentence):
     lent = len(sentence)
     if lent == 0:
-        n_turple = (lent, None)
+        n_tuple = (lent, None)
     else:
-        n_turple = (lent, sentence[0])
-    return n_turple
+        n_tuple = (lent, sentence[0])
+    return n_tuple
