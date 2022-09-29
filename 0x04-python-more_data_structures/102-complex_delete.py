65692b4893852e4cfa18d8a38e82d93f7e0551f7
David Adejumo <71224787+KingDayveed9c@users.noreply.github.com>
2022-09-29 02:48:11 +0100

    Data struct

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
-
-def complex_delete(a_dict, value):
-
-    if a_dict is None:
-        return None
-    del_key = None
-    keys = tuple(a_dict.keys())
-    for key in keys:
-        if a_dict[key] == value:
-            del a_dict[key]
-    return a_dict
+def complex_delete(a_dictionary, value):
+    keys_to_del = []
+    for key in a_dictionary:
+        if a_dictionary[key] == value:
+            keys_to_del.append(key)
+    for key in keys_to_del:
+        del a_dictionary[key]
+    return a_dictionary
